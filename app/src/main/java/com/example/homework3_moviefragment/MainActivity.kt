777c11a01c689bada8568376a5629aefd6aa93c0
dan-codes <dli123@syr.edu>
2019-10-04 15:26:26 -0400

    back button works

--- app/src/main/java/com/example/homework3_moviefragment/MainActivity.kt
+++ app/src/main/java/com/example/homework3_moviefragment/MainActivity.kt
@@ -6,6 +6,7 @@ import android.util.Log
 import android.view.MenuItem
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
 import androidx.navigation.ui.setupActionBarWithNavController
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -16,23 +17,34 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
         navController = Navigation.findNavController(this, R.id.nav_host_fragment)
-        //setupSideNavigationMenu(navController)
         val appBar = supportActionBar
-        appBar?.setDisplayHomeAsUpEnabled(true)
+
+//        appBar?.setDisplayHomeAsUpEnabled(true)
+        //setupSideNavigationMenu(navController)
         //actionBar.hide()
+        NavigationUI.setupActionBarWithNavController(this,navController)
     }
 
-    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
-        return super.onOptionsItemSelected(item)
-        when (item?.itemId)
-        {
-            android.R.id.home -> {
-                Log.d("debug", "back button pressed")
-                navController.navigateUp()
-                return true
-            }
-        }
+    override fun onSupportNavigateUp(): Boolean {
+        navController.navigateUp()
+        return super.onSupportNavigateUp()
     }
+
+//    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+//        return super.onOptionsItemSelected(item)
+//        when (item?.itemId)
+//        {
+//            android.R.id.home -> {
+//                Log.d("debug", "back button pressed")
+//                navController.navigateUp()
+//                return true
+//            }
+//            R.id.aboutMe -> {
+//                val appBar = supportActionBar
+//                appBar?.setDisplayHomeAsUpEnabled(true)
+//            }
+//        }
+//    }
 
 
 }
